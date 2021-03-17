84b09bd3b61417fab483acf9f1a38e0cf6b95a80
jacksonmoreira <jacksonmoreiraneto@gmail.com>
2021-03-16 21:53:42 -0300

    Update Jogo da adivinhação.py

--- Exercicios/Jogo da adivinhação.py
+++ Exercicios/Jogo da adivinhação.py
@@ -1,25 +1,16 @@
-# Libs
 from random import randint
-
-# Cabeçalho
 print('#' * 29)
 print('     JOGO DA ADIVINHAÇÃO     ')
 print('#' * 29)
-
-# Informações
 print('-' * 15)
 print('''Esse é o jogo da adivinhação, eu vou
 pensar em um número inteiro de 0 a 5, mas,
 você terá que adivinhar em que número eu pensei.
 VAMOS LÁ?''')
 print('-' * 15)
-
-# Variáveis
 print('Começando jogo...')
 num1 = int(input('Digite o número em que eu estou pensando: ').strip())
 ale = randint(0, 5)
-
-# Operações
 print('Vocẽ acha que eu estava pensando no número {}, vamos ver se você está correto.'.format(num1))
 print('-' * 15)
 print('O número em que eu estava pensando é {}.'.format(ale))
